adab6b6c3f7dbb2c4c456e2b5a47e660bbede93e
Rakendra <aznil.rakendra@gmail.com>
2026-04-06 01:53:06 +0700

    menambahkan class SayaMusicTrack

--- TP_MODUL6_103022400092/Program.cs
+++ TP_MODUL6_103022400092/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+
+namespace Modul6_KPL
+{
+    public class SayaMusicTrack
+    {
+        private int id;
+        private int playCount; 
+        private string title;
+
+        public SayaMusicTrack(string title)
+        {
+            this.title = title;
+            this.playCount = 0; 
+
+            Random random = new Random();
+            this.id = random.Next(10000, 100000);
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine("ID Track   : " + this.id);
+            Console.WriteLine("Judul Lagu : " + this.title);
+            Console.WriteLine("Play Count : " + this.playCount);
+            Console.WriteLine("-------------------------------------------------");
+        }
+    }
+}
